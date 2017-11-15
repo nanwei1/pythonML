88775dc4423b0a6c50dad8b54fd4fa10f650a5ce
Nan <erik.weinan@gmail.com>
2017-11-15 18:38:19 -0500

    updated files" preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -13,3 +13,33 @@ print("Std dev =", data_standardized.std(axis=0))
 data_scaler = preprocessing.MinMaxScaler(feature_range=(0,1))
 data_scaled = data_scaler.fit_transform(data)
 print("\nMin max scaled data =", data_scaled)
+
+# normalization
+data_normalized = preprocessing.normalize(data, norm='l1')
+print("\nL1 normalized data =", data_normalized)
+
+# binarization
+data_binarized = preprocessing.Binarizer(threshold=1.4)\
+    .transform(data)
+print("\nBinarized data", data_binarized)
+
+# one hot encoding
+encoder = preprocessing.OneHotEncoder()
+data1 = [[0, 2, 1, 12], [1, 3, 5, 3], [2, 3, 2, 12], [1, 2, 4,
+3]]
+encoder.fit(data1)
+encoded_vector = encoder.transform([[2,3,5,3]]).toarray()
+print(data1)
+print("\nEncoded vector =", encoded_vector)
+
+# label encoding
+label_encoder = preprocessing.LabelEncoder()
+input_classes = ['Audi', 'Ford', 'Audi', 'Toyota', 'Ford', 'BMW']
+label_encoder.fit(input_classes)
+print("\nClass mapping:")
+for i,item in enumerate(label_encoder.classes_):
+    print(item, '-->', i)
+labels=['Toyota','Ford','Audi']
+encoded_labels = label_encoder.transform(labels)
+print("\nLabels = ", labels)
+print("Encoded labels = ", list(encoded_labels))
